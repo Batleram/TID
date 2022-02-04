296e5dca7abc8997c406f4044991e7b870feede6
batleram <batleram@gmail.com>
2022-02-04 15:03:49 -0500

    communication between frontend and backend and db established

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const App = () => {
-    return <h1>Test</h1>;
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        //
+        fetch(`http://${window.location.hostname}:7676/getthings`, {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .then((res) => setData(res))
+            .catch((e) => console.error(e));
+    }, []);
+
+    return (
+        <>
+            <h1>Test</h1>
+            <div>{JSON.stringify(data[0])}</div>
+        </>
+    );
 };
